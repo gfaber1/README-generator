9a6e1c0cb3e32b33e89fef29c8f15131674ddb7a
Gabe Faber <gmfaber55@gmail.com>
2023-02-15 11:05:13 -0700

    added an array of questions that will be asked to develope the readme file

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
-
+const Inquirer = require('inquirer')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description of your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide a step by step explanation of how to install your project',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples on how to use your application',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'List collaborators and sources that assisted you in building your application',
+        name: 'credits',
+    },
+    {
+        type: 'list',
+        message: 'What license does your project use?',
+        choices: ['MIT', 'GPLv2', 'Apache', 'Other'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Provide a list of features of your project',
+        name: 'features',
+    },
+    {
+        type: 'input',
+        message: 'If you would like other developers to contribute to your project, provide some guidelines for them to follow',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'If you have written tests for you application, provide examples of how to run them here',
+        name: 'tests',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
